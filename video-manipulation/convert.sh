5e6e8fac14c2e7da90a83ccf6a8437597ed6821c
Mohammad Atef <mohammad.atef@trustious.com>
2015-05-10 08:29:22 +0200

    Fixing command

--- video-manipulation/convert.sh
+++ video-manipulation/convert.sh
@@ -4,7 +4,7 @@ IFS=$(echo -en "\n\b")
 mkdir result
 for s in *; do
     echo "$s"
-    avconv -i "$s" -qscale 7 -strict experimental "result/$s"
+    avconv -i "$s" -qscale 13 -strict experimental "result/$s.mp4"
     mkvextract tracks "$s" 3:"result/$s.ssa"
 done
 
